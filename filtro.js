fdeb33b251b67ff8649153dff1c36e41df4965ee
Thiago Gonçalves <106316927+thiagooncalves@users.noreply.github.com>
2022-06-30 17:04:09 -0300

    mudança

--- filtro.js
+++ filtro.js
@@ -6,7 +6,7 @@ function Filtrar() {
     table = document.getElementById("tabela-dados");
     tr = table.getElementsByTagName("tbody")[0].rows;
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0, 1];
+      td = tr[i].getElementsByTagName("td")[0];
       if (td) {
         if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
           tr[i].style.display = "";
