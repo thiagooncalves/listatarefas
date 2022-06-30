6ff458293c44b4173cc93120a8524723917e6fe8
Thiago Gonçalves <106316927+thiagooncalves@users.noreply.github.com>
2022-06-30 17:30:59 -0300

    mudança

--- filtro.js
+++ filtro.js
@@ -1,18 +1,19 @@
 
 function Filtrar() {
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("filtro-tabela");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("tabela-dados");
-    tr = table.getElementsByTagName("tbody")[0].rows;
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }       
-    }
+  var input, filter, table, tr, tdCol1, tdCol2, i;
+  input = document.getElementById("filtro-tabela");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("tabela-dados");
+  tr = table.getElementsByTagName("tbody")[0].rows;
+  for (i = 0; i < tr.length; i++) {
+    tdCol1 = tr[i].getElementsByTagName("td")[0];
+    tdCol2 = tr[i].getElementsByTagName("td")[1];
+    if (tdCol1 || tdCol1) {
+      if (tdCol1.innerHTML.toUpperCase().indexOf(filter) > -1 || tdCol2.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
   }
+}
