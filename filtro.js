b263aac8cfe3f91ac57b78534406132b8a136ece
Thiago Gonçalves <106316927+thiagooncalves@users.noreply.github.com>
2022-07-05 09:54:08 -0300

    mudança

--- filtro.js
+++ filtro.js
@@ -3,6 +3,6 @@ function Filtrar() {
   inputFilter = document.getElementById("filtro-tabela");
   filter = inputFilter.value.toUpperCase();
   if (filter.length >= 1 && filter.length <= 3 ) return
-  var tarefasFiltradas = tarefas.filter(function(obj){ return (obj.titulo.toUpperCase().indexOf(filter) > -1 || obj.descricao.toUpperCase().indexOf(filter) > -1); });
+  let tarefasFiltradas = tarefas.filter(function(obj){ return (obj.titulo.toUpperCase().indexOf(filter) > -1 || obj.descricao.toUpperCase().indexOf(filter) > -1); });
   renderizaTabela(tarefasFiltradas);
 }
