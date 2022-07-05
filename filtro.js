a44a5ce81d1895b3816235e9f4b73e3e26f83439
Thiago Gonçalves <106316927+thiagooncalves@users.noreply.github.com>
2022-07-05 09:42:08 -0300

    mudanças

--- filtro.js
+++ filtro.js
@@ -1,30 +1,8 @@
-
 function Filtrar() {
-  let inputFilter, filter, table, tr, tdTitulo, tdDescricao, indiceTarefas;
+  let inputFilter;
   inputFilter = document.getElementById("filtro-tabela");
   filter = inputFilter.value.toUpperCase();
   if (filter.length >= 1 && filter.length <= 3 ) return
-  table = document.getElementById("tabela-dados");
-  tr = table.getElementsByTagName("tbody")[0].rows;
-  for (indiceTarefas = 0; indiceTarefas < tr.length; indiceTarefas++) {
-    tdTitulo = tr[indiceTarefas].getElementsByTagName("td")[0];
-    tdDescricao = tr[indiceTarefas].getElementsByTagName("td")[1];
-    if (tdTitulo) {
-      if (tdTitulo.innerHTML.toUpperCase().indexOf(filter) > -1 || tdDescricao.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[indiceTarefas].style.display = "";
-      } else {
-        tr[indiceTarefas].style.display = "none";
-      }
-    }       
-  }
-}
-
-
-function Filtrar() {
-let inputFilter;
-inputFilter = document.getElementById("filtro-tabela");
-filter = inputFilter.value.toUpperCase();
-if (filter.length >= 1 && filter.length <= 3 ) return
-var tarefasFiltradas = tarefas.filter(function(obj){ return (obj.titulo.toUpperCase().indexOf(filter) > -1 || obj.descricao.toUpperCase().indexOf(filter) > -1); });
-renderizaTabela(tarefasFiltradas);
+  var tarefasFiltradas = tarefas.filter(function(obj){ return (obj.titulo.toUpperCase().indexOf(filter) > -1 || obj.descricao.toUpperCase().indexOf(filter) > -1); });
+  renderizaTabela(tarefasFiltradas);
 }
