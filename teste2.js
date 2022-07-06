647226cf831132124db13a0b5046b8b2eb502280
Thiago Gonçalves <106316927+thiagooncalves@users.noreply.github.com>
2022-07-06 17:38:25 -0300

    adição de arquivos

--- teste2.js
+++ teste2.js
@@ -1,18 +1,9 @@
-function Filtrar() {
-  let inputFilter;
-  inputFilter = document.getElementById("filtro-tarefas");
-  filter = inputFilter.value.toUpperCase();
-  if (filter.length >= 1 && filter.length <= 3 ) return
-  let tarefasFiltradas = tarefas.filter(function(obj){ return (obj.titulo.toUpperCase().indexOf(filter) > -1 || obj.descricao.toUpperCase().indexOf(filter) > -1); });
-  renderizaTabela(tarefasFiltradas);
-}
-
-let nome = prompt('Qual é o seu nome?');
+let nome = window.prompt('Qual é o seu nome?');
 document.write(`Olá, <strong>${nome}</strong>, seu nome tem <strong>${nome.length}</strong> letras.<br>`)
 document.write(`Seu nome em maiúsculo é ${nome.toUpperCase()}<br>`)
 document.write(`Seu nome em minúsculo é ${nome.toLowerCase()}<br>`)
 
-let boxPreço = prompt('Qual é o preço do seu terno?');
+let boxPreço = window.prompt('Qual é o preço do seu terno?');
 let inputPreço = 1999
 let moeda = inputPreço.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
 document.write(`O preço do seu terno é: ${moeda}`);
@@ -26,7 +17,4 @@ salario >= 3000 || sexo != 'M' // o salário é maior ou igual a 3000 ou o sexo 
 estado == 'MG' || estado == 'SC' // o estado é MG ou o estado é SC?
 
 let media = 7
-media >= 7 ?'Aprovado' :'Reprovado' // o resultado será 'aprovado'.
-
-let idade = 27
-let thiago = idade == 27 ?'1995' :'Error' // o resultado será '1995'.
+media >= 7 ?'Aprovado' :'Reprovado'
